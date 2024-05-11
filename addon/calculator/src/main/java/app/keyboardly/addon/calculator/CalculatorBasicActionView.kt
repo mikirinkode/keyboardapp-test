4113f7b77294dd59cb5ed3c42afeefa7b6215c8f
Muhammad Wafa <muhammadwafa105@gmail.com>
2024-05-11 20:26:44 +0700

    setup: jetpack compose on xml

--- addon/calculator/src/main/java/app/keyboardly/addon/calculator/CalculatorBasicActionView.kt
+++ addon/calculator/src/main/java/app/keyboardly/addon/calculator/CalculatorBasicActionView.kt
@@ -1,5 +1,10 @@
 package app.keyboardly.addon.calculator
 
+import androidx.compose.foundation.clickable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import app.keyboardly.addon.calculator.databinding.CalculatorBasicLayoutBinding
 import app.keyboardly.lib.KeyboardActionDependency
 import app.keyboardly.lib.KeyboardActionView
@@ -16,9 +21,19 @@ class CalculatorBasicActionView(
             backButton.setOnClickListener {
                 dependency.viewAddOnNavigation()
             }
+
+            composeView.apply {
+                setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+                setContent {
+                    MaterialTheme {
+                        Text("Hello from Compose!", modifier = Modifier.clickable(onClick = {
+                            dependency.commitText("Hello from Compose")
+                        }))
+                    }
+                }
+            }
         }
     }
-
     override fun onResume() {
         super.onResume()
     }
